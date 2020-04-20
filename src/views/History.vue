5d84c43e4365f3217ca1701e96338f86e51607f2
bogdan demchenko <bogdandemchenko@gmail.com>
2020-04-20 22:51:17 +0300

    detail page | chart

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
         </div>
 
         <div class="history-chart">
-            <canvas></canvas>
+            <canvas ref="canvas"></canvas>
         </div>
 
         <Loader v-if="loading" />
@@ -36,9 +36,11 @@
 <script>
     import HistoryTable from "../components/HistoryTable"
     import paginationMixin from '../mixins/pagination.mixin'
+    import {Pie} from 'vue-chartjs'
 
     export default {
         name: "History",
+        extends: Pie,
         mixins: [paginationMixin],
         data: () => ({
             loading: true,
@@ -48,17 +50,40 @@
             this.records = await this.$store.dispatch('fetchRecords')
             const categories = await this.$store.dispatch('fetchCategories')
 
-            this.setupPagination(this.records.map(record => {
-                return {
-                    ...record,
-                    categoryName: categories
-                        .find(c => c.id === record.categoryId).title,
-                    typeClass: record.type === 'income' ? 'green' : 'red',
-                    typeText: record.type === 'income' ? 'Доход' : 'Расход',
-                }
-            }))
+            this.setup(categories)
 
             this.loading = false
+        },
+        methods: {
+            setup(categories) {
+                this.setupPagination(this.records.map(record => {
+                    return {
+                        ...record,
+                        categoryName: categories
+                            .find(c => c.id === record.categoryId).title,
+                        typeClass: record.type === 'income' ? 'green' : 'red',
+                        typeText: record.type === 'income' ? 'Доход' : 'Расход',
+                    }
+                }))
+
+                this.renderChart({
+                    labels: categories.map(c => c.title),
+                    datasets: [
+                        {
+                            label: 'Расходы по категориям',
+                            backgroundColor: '#f87979',
+                            data: categories.map(c => {
+                                return this.records.reduce((total, r) => {
+                                    if (r.categoryId === c.id && r.type === 'outcome') {
+                                        total += +r.amount
+                                    }
+                                    return total
+                                }, 0)
+                            })
+                        }
+                    ]
+                })
+            }
         },
         components: {
             HistoryTable
