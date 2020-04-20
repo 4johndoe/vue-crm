ba7fd690940fb42ff22a42a84de1f0f3a1fb1500
bogdan demchenko <bogdandemchenko@gmail.com>
2020-04-20 21:02:05 +0300

    guard routes, error in main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -23,6 +23,8 @@
 <script>
     import Navbar from "../components/app/Navbar";
     import Sidebar from "../components/app/Sidebar";
+    import messages from "../utils/messages";
+
     export default {
         name: 'main-layout',
         data: () => ({
@@ -35,6 +37,16 @@
             }
 
             this.loading = false
+        },
+        computed: {
+            error() {
+                return this.$store.getters.error
+            }
+        },
+        watch: {
+            error(fbError) {
+                this.$error(messages[fbError.code] || 'Что-то пошло не так')
+            }
         },
         components: {
             Sidebar, Navbar
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,4 +1,5 @@
 export default {
+    'login': 'Для начала войдите в систему',
     'logout': 'Вы вышли из системы',
     'auth/user-not-found': 'Пользователь не найден',
     "auth/wrong-password": 'Неверный пароль',
