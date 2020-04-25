0e4651ccb88dffc83b45db1ae6ca6f52eb94e3ef
bogdan demchenko <bogdandemchenko@gmail.com>
2020-04-25 23:17:38 +0300

    fix currency

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="page-title">
             <h3>{{ 'Planning_Title' | localize }}</h3>
-            <h4>{{info.bill | currency(baseCurrency)}}</h4>
+            <h4>{{info.bill | currency}}</h4>
         </div>
 
         <Loader v-if="loading"/>
@@ -18,7 +18,7 @@
             <div v-for="cat in categories" :key="cat.id">
                 <p>
                     <strong>{{cat.title}}:</strong>
-                    {{cat.spend | currency(baseCurrency)}} из {{cat.limit | currency(baseCurrency)}}
+                    {{cat.spend | currency }} из {{cat.limit | currency }}
                 </p>
                 <div class="progress" v-tooltip="cat.tooltip">
                     <div
@@ -46,8 +46,7 @@
         name: "Planning",
         data: () => ({
             loading: true,
-            categories: [],
-            baseCurrency: 'UAH'
+            categories: []
         }),
         computed: {
             ...mapGetters(['info'])
